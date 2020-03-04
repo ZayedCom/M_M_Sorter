d649a597752830d0140a6df76b9e562cee10ef77
Zaid K. Al Qassar <WGS.Xperia@gmail.com>
2020-03-04 14:13:55 +0300

    Alpha 2.0

--- app/src/main/java/com/app_nfusion/mmsorter/ui/about/AboutFragment.java
+++ app/src/main/java/com/app_nfusion/mmsorter/ui/about/AboutFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,11 +24,96 @@ public class AboutFragment extends Fragment {
         aboutViewModel =
                 ViewModelProviders.of(this).get(AboutViewModel.class);
         View root = inflater.inflate(R.layout.fragment_about, container, false);
+
         final TextView textView = root.findViewById(R.id.text_about);
+        final TextView textViewLeadProgrammer = root.findViewById(R.id.text_lead_programmer);
+        final TextView textViewZaid = root.findViewById(R.id.text_zaid);
+        final TextView textViewOutputProgrammer = root.findViewById(R.id.text_output_programmer);
+        final TextView textViewMamadou = root.findViewById(R.id.text_mamadou);
+        final TextView textViewMotorProgrammer = root.findViewById(R.id.text_motor_programmer);
+        final TextView textViewBusra = root.findViewById(R.id.text_busra);
+        final TextView textViewIntegration = root.findViewById(R.id.text_integration);
+        final TextView textViewAhmed = root.findViewById(R.id.text_ahmed);
+        final TextView textViewDesign = root.findViewById(R.id.text_design);
+        final TextView textViewOmar = root.findViewById(R.id.text_omar);
+
+        final ScrollView scrollView = (ScrollView) root.findViewById(R.id.scrollView);
+        scrollView.setEnabled(true);
+
         aboutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextLeadProgrammer().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewLeadProgrammer.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextZaid().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewZaid.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextOutputProgrammer().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewOutputProgrammer.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextMamadou().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewMamadou.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextMotorProgrammer().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewMotorProgrammer.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextBusra().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewBusra.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextIntegration().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewIntegration.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextAhmed().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewAhmed.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextDesign().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewDesign.setText(s);
+            }
+        });
+
+        aboutViewModel.getTextOmar().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textViewOmar.setText(s);
             }
         });
         return root;
--- app/src/main/java/com/app_nfusion/mmsorter/ui/about/AboutViewModel.java
+++ app/src/main/java/com/app_nfusion/mmsorter/ui/about/AboutViewModel.java
@@ -7,13 +7,84 @@ import androidx.lifecycle.ViewModel;
 public class AboutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<String> mTextLeadProgrammer;
+    private MutableLiveData<String> mTextZaid;
+    private MutableLiveData<String> mTextOutputProgrammer;
+    private MutableLiveData<String> mTextMamadou;
+    private MutableLiveData<String> mTextMotorProgrammer;
+    private MutableLiveData<String> mTextBusra;
+    private MutableLiveData<String> mTextIntegration;
+    private MutableLiveData<String> mTextAhmed;
+    private MutableLiveData<String> mTextDesign;
+    private MutableLiveData<String> mTextOmar;
 
     public AboutViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Bahcesehir University");
+        mTextLeadProgrammer = new MutableLiveData<>();
+        mTextZaid = new MutableLiveData<>();
+        mTextOutputProgrammer = new MutableLiveData<>();
+        mTextMamadou =  new MutableLiveData<>();
+        mTextMotorProgrammer = new MutableLiveData<>();
+        mTextBusra = new MutableLiveData<>();
+        mTextIntegration = new MutableLiveData<>();
+        mTextAhmed = new MutableLiveData<>();
+        mTextDesign = new MutableLiveData<>();
+        mTextOmar = new MutableLiveData<>();
+
+        mText.setValue("M&M Sorter Capstone Project");
+        mTextLeadProgrammer.setValue("Color Detecting Algorithm & Mobile Developer");
+        mTextZaid.setValue("Zaid K. Al Qassar");
+        mTextOutputProgrammer.setValue("Output & Display Programmer");
+        mTextMamadou.setValue("Mamadou Diop");
+        mTextMotorProgrammer.setValue("Motor Programmer");
+        mTextBusra.setValue("Büşra Çelik");
+        mTextIntegration.setValue("Integration of the Project");
+        mTextAhmed.setValue("Ahmed Tariq Abdalaal Aboseif");
+        mTextDesign.setValue("Design");
+        mTextOmar.setValue("Omar Fakhereldin");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<String> getTextLeadProgrammer() {
+        return mTextLeadProgrammer;
+    }
+
+    public LiveData<String> getTextZaid() {
+        return mTextZaid;
+    }
+
+    public LiveData<String> getTextOutputProgrammer() {
+        return mTextOutputProgrammer;
+    }
+
+    public LiveData<String> getTextMamadou() {
+        return mTextMamadou;
+    }
+
+    public LiveData<String> getTextMotorProgrammer() {
+        return mTextMotorProgrammer;
+    }
+
+    public LiveData<String> getTextBusra() {
+        return mTextBusra;
+    }
+
+    public LiveData<String> getTextIntegration() {
+        return mTextIntegration;
+    }
+
+    public LiveData<String> getTextAhmed() {
+        return mTextAhmed;
+    }
+
+    public LiveData<String> getTextDesign() {
+        return mTextDesign;
+    }
+
+    public LiveData<String> getTextOmar() {
+        return mTextOmar;
     }
 }
