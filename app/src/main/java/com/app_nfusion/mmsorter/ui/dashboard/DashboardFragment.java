2e152472aac6edef607d99e2e1e6b5c537e95767
Zaid K. Al Qassar <WGS.Xperia@gmail.com>
2020-03-07 02:54:56 +0300

    Alpha 5.5

--- app/src/main/java/com/app_nfusion/mmsorter/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/app_nfusion/mmsorter/ui/dashboard/DashboardFragment.java
@@ -1,10 +1,15 @@
 package com.app_nfusion.mmsorter.ui.dashboard;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +19,15 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.app_nfusion.mmsorter.R;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.app_nfusion.mmsorter.ui.dashboard.DashboardViewModel.startTimer;
+
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+
+    boolean startSwitch = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +35,50 @@ public class DashboardFragment extends Fragment {
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        final Button stopButton = root.findViewById(R.id.buttonStop);
+        final Button startButton = root.findViewById(R.id.buttonStart);
+        final Button statsButton = root.findViewById(R.id.buttonStats);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startSwitch == false) {
+                    startTimer(textView, false);
+                    startButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_pause_white_24dp, 0, 0);
+                    startButton.setText("Pause");
+                    Toast.makeText(getActivity(), "Initiating M&M Sorter", Toast.LENGTH_LONG).show();
+                    startSwitch = true;
+                }
+                else if (startSwitch == true) {
+                    startTimer(textView, true);
+                    startButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_play_arrow_white_24dp, 0, 0);
+                    startButton.setText("Resume");
+                    Toast.makeText(getActivity(), "Pausing M&M Sorter", Toast.LENGTH_LONG).show();
+                    startSwitch = false;
+                }
+            }
+        });
+
+        statsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return root;
     }
 }
--- app/src/main/java/com/app_nfusion/mmsorter/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/app_nfusion/mmsorter/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,80 @@
 package com.app_nfusion.mmsorter.ui.dashboard;
 
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
+import com.app_nfusion.mmsorter.R;
+
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 public class DashboardViewModel extends ViewModel {
+
+    static CountDownTimer timer;
 
     private MutableLiveData<String> mText;
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Sorting M&M's");
+        mText.setValue("Press start to initiate the sorting");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    static public void startTimer(final TextView textView, boolean pauseTimer) {
+        final long time = 600000;
+        final long interval = 1000;
+        final long[] tempTime = new long[1];
+
+        if (pauseTimer == false) {
+            timer = new CountDownTimer(time, interval) {
+                @SuppressLint({"DefaultLocale", "SetTextI18n", "ResourceAsColor"})
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    textView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                    textView.setTextColor(R.color.colorAccent);
+                    textView.setText("" + String.format("%d : %02d",
+                            TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                    tempTime[0] = millisUntilFinished;
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
+        }
+        else if (pauseTimer == true){
+            timer.cancel();
+            timer = new CountDownTimer(tempTime[0], interval) {
+                @SuppressLint({"DefaultLocale", "SetTextI18n", "ResourceAsColor"})
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    textView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                    textView.setTextColor(R.color.colorAccent);
+                    textView.setText("" + String.format("%d : %02d",
+                            TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                    tempTime[0] = millisUntilFinished;
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
+        }
     }
 }
